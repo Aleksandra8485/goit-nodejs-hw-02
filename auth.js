0cae2fbe4720d1a904ec90c198863d6a7ec91b5d
Aleksandra <a.walczak1984@gmail.com>
2023-09-14 13:11:23 +0200

    changes in app.js, auth.js

--- auth.js
+++ auth.js
@@ -1,45 +1,90 @@
-const jwt = require("jsonwebtoken");
-const User = require("./services/users"); // Importuj model użytkownika
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const User = require("./services/users");
+const dotenv = require("dotenv");
 
-// Funkcja do generowania tokena JWT po prawidłowym uwierzytelnieniu
-const generateToken = (user) => {
-  const payload = {
-    userId: user._id,
-    email: user.email,
-  };
-  const options = {
-    expiresIn: "1h", // Czas ważności tokenu
-  };
-  return jwt.sign(payload, "your-secret-key", options); // Zmień 'your-secret-key' na własny klucz
+dotenv.config();
+const secret = process.env.SECRET;
+
+const ExtractJWT = passportJWT.ExtractJwt;
+const JWTStrategy = passportJWT.Strategy;
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: secret,
+    },
+    async (jwtPayload, done) => {
+      try {
+        const user = await User.findById(jwtPayload.id).select("-password");
+        console.log(user);
+        if (!user) {
+          return done(null, false, { message: "Not authorized" });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error, false);
+      }
+    }
+  )
+);
+
+const auth = (req, res, next) => {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    if (err || !user) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
 };
 
-// Funkcja do logowania użytkownika
-const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+module.exports = auth;
 
-    // Sprawdź, czy istnieje użytkownik o podanym e-mailu
-    const user = await User.findOne({ email });
+// const jwt = require("jsonwebtoken");
+// const User = require("./services/users"); // Importuj model użytkownika
 
-    if (!user) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
+// // Funkcja do generowania tokena JWT po prawidłowym uwierzytelnieniu
+// const generateToken = (user) => {
+//   const payload = {
+//     userId: user._id,
+//     email: user.email,
+//   };
+//   const options = {
+//     expiresIn: "1h", // Czas ważności tokenu
+//   };
+//   return jwt.sign(payload, "your-secret-key", options); // Zmień 'your-secret-key' na własny klucz
+// };
 
-    // Sprawdź hasło
-    const isPasswordValid = await user.checkPassword(password);
+// // Funkcja do logowania użytkownika
+// const login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
 
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
+//     // Sprawdź, czy istnieje użytkownik o podanym e-mailu
+//     const user = await User.findOne({ email });
 
-    // Wygeneruj token JWT
-    const token = generateToken(user);
+//     if (!user) {
+//       return res.status(401).json({ message: "Invalid credentials" });
+//     }
 
-    // Odeślij token jako odpowiedź
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
+//     // Sprawdź hasło
+//     const isPasswordValid = await user.checkPassword(password);
 
-module.exports = { login };
+//     if (!isPasswordValid) {
+//       return res.status(401).json({ message: "Invalid credentials" });
+//     }
+
+//     // Wygeneruj token JWT
+//     const token = generateToken(user);
+
+//     // Odeślij token jako odpowiedź
+//     res.status(200).json({ token });
+//   } catch (error) {
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// };
+
+// module.exports = { login };
