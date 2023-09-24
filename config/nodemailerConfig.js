6507ef40f7fa5130382364584f9c1f51f389fb9d
Aleksandra <a.walczak1984@gmail.com>
2023-09-24 21:35:39 +0200

    changes in login, env

--- config/nodemailerConfig.js
+++ config/nodemailerConfig.js
@@ -3,9 +3,32 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER, // Ustaw adres e-mail jako zmienną środowiskową
-    pass: process.env.EMAIL_PASSWORD, // Ustaw hasło jako zmienną środowiskową
+    user: process.env.EMAIL_LOGIN,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
-module.exports = transporter;
+const sendVerificationEmail = (email, verificationToken) => {
+  const verificationLink = `http://localhost:3000/api/users/verify/${verificationToken}`; // Używaj 3002 jeśli to jest poprawny port serwera
+  const mailOptions = {
+    from: process.env.EMAIL_LOGIN,
+    to: email,
+    subject: "Weryfikacja adresu email",
+    text: `Kliknij w poniższy link, aby zweryfikować swój adres email: ${verificationLink}`,
+  };
+  console.log(verificationLink);
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email", error);
+    } else {
+      console.log("Email sent:", info.response);
+    }
+  });
+};
+
+module.exports = {
+  sendVerificationEmail,
+};
